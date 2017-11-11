8e43fb07629ed9816f6f3d1b968256129bece77b
Andrew <andrew.strycharz@gmail.com>
2017-11-11 15:54:01 -0500

    update inf file

---
+++ f1.php
@@ -0,0 +1,13 @@
+total 4
+drwxr-xr-x 2 andrew andrew 4096 Nov 11 12:54 bar
+-rw-r--r-- 1 andrew andrew    0 Nov 11 12:59 f1.php
+-rw-r--r-- 1 andrew andrew    0 Nov 11 12:59 file2.pHp
+-rw-r--r-- 1 andrew andrew    0 Nov 11 12:59 file3.php
+-rw-r--r-- 1 andrew andrew    0 Nov 11 12:59 hermit.cpp
+-rw-r--r-- 1 andrew andrew    0 Nov 11 12:59 hermit.o
+-rw-r--r-- 1 andrew andrew    0 Nov 11 12:59 index.html
+-rw-r--r-- 1 andrew andrew    0 Nov 11 12:59 index.php
+-rw-r--r-- 1 andrew andrew    0 Nov 11 12:59 inf2.txt
+-rw-r--r-- 1 andrew andrew    0 Nov 11 12:59 inf.txt
+-rw-r--r-- 1 andrew andrew    0 Nov 11 12:59 test.log
+-rw-r--r-- 1 andrew andrew    0 Nov 11 12:59 version.log
